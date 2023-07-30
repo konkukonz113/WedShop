0f733fc71ee2890740b694782bdd31404961f7ab
konkukonz113 <huytruonghn98@gmail.com>
2023-07-30 07:56:16 +0700

    init project

---
+++ src/main/java/com/example/wedshop/WedshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.wedshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedshopApplication.class, args);
+	}
+
+}
